54953f188e68d8d654b80e3dc271d112cd6fdaae
Matthew Owens (1000028615) <OWENMG2@student.op.ac.nz>
2017-08-03 13:51:16 +1200

    Step 4

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -12,9 +12,26 @@ app.config.update(dict(
     USERNAME='admin',
     PASSWORD='default'
 ))
+
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 def connect_db():
     """Connects to the specific database."""
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+        
