729a0332ccc91c6d3b6a59f1695ba01f7c496af7
Matthew Owens (1000028615) <OWENMG2@student.op.ac.nz>
2017-08-03 13:57:51 +1200

    Step 5

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -33,5 +33,15 @@ def close_db(error):
     """Closes the database again at the end of the request."""
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
+def init_db():
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
 
+@app.cli.command('initdb')
+def initdb_command():
+    """Initializes the database."""
+    init_db()
+    print('Initialized the database.')
         
